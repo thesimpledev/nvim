505d2a029a2d744f5c877233624926d942863891
sstanton <steven@thesimpledev.com>
2024-12-23 21:38:16 -0500

    Initial Upload

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Set relative line numbers
+vim.opt.relativenumber = true
+
+-- Also show the absolute number on the current line
+vim.opt.number = true
+
+-- Enable system clipboard access
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
